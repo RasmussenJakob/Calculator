de6cf08c99461c0e5d65241416457b6725433f35
jakob.rasmussen <jakob.rasmussen@MU-C1-16.mullen.pvt.k12.co.us>
2017-01-24 08:29:12 -0700

    initial commit

---
+++ Calculator/src/calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class calculator
+	{
+		static Scanner userInput;
+		
+		public static void main(String[] args)
+			{
+				userIn();
+				add();
+				sub();
+				mult();
+				div();
+			}
+		
+		public static void userIn()
+		{
+			
+		}
+
+		public static double add(double addnum1, double addnum2)
+			{
+				System.out.println(add(2, 3));
+				return addnum1 + addnum2;
+			}
+			
+		public static double sub(double subnum1, double subnum2)
+			{
+				System.out.println(add(2, 3));
+				return subnum1 - subnum2; 
+			}
+			
+		public static double mult(double multnum1, double multnum2)
+			{
+				System.out.println(add(2, 3));
+				return multnum1 * multnum2;
+			}
+			
+		public static double div(double divnum1, double divnum2)
+			{
+				System.out.println(add(2, 3));
+				return divnum1 / divnum2;
+			}
+	}
